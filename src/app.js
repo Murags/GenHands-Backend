01ce79b2737609447357b507f2dc5cd1b27cca7b
Dennis Murage <dmurage140@gmail.com>
2025-05-14 19:46:28 +0300

    Enable cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 // dotenv/config is preloaded via the npm script
 import express from 'express';
 import connectDB from './config/db.js';
+import cors from 'cors';
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('API Running'));
 
